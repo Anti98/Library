041eb68f07da450d90c6982285b953b2c014ee67
Andrey Shalyapin <aanti98@gmail.com>
2022-08-31 20:56:29 +0300

    addet kek

--- src/main/java/com/shalyapin/library/service/LibraryService.java
+++ src/main/java/com/shalyapin/library/service/LibraryService.java
@@ -15,33 +15,41 @@ public class LibraryService {
     private final LibraryRepository libraryRepository;
     private final AuthorRepository authorRepository;
 
-    public Library createLibrary(Library library){
+    public Library createLibrary(Library library) {
         return libraryRepository.save(library);
     }
-    public Library getLibrary(Long id){
-        return libraryRepository.findById(id).orElseThrow(()->new LibraryException("No library with such id"));
+
+    public Library getLibrary(Long id) {
+        System.out.println("KEKE");
+        return libraryRepository.findById(id).orElseThrow(() -> new LibraryException("No library with such id"));
     }
-    public Library updateLibrary(Long id,Library newLibrary){
-        Library library=libraryRepository.findById(id).orElseThrow(()->new LibraryException("No library with such id")); //Нужно ли проверять наличие?
+
+    public Library updateLibrary(Long id, Library newLibrary) {
+        Library library = libraryRepository.findById(id).orElseThrow(() -> new LibraryException("No library with such id")); //Нужно ли проверять наличие?
         newLibrary.setId(id);
         return libraryRepository.save(newLibrary);
     }
-    public void deleteLibrary(Long id){ //Нужно ли проверять наличие данной бибилотеки? Нужно ли возвращать булиан или достаточно войда?
+
+    public void deleteLibrary(Long id) { //Нужно ли проверять наличие данной бибилотеки? Нужно ли возвращать булиан или достаточно войда?
         libraryRepository.deleteById(id);
     }
-    public List<Library> getAllLibraries(){
+
+    public List<Library> getAllLibraries() {
         return libraryRepository.findAll();
 
     }
+
     //TODO Сделать возврат по содержанию слова а не полному соответсвтию
-    public List<Library> getLibraryByName(String name){
+    public List<Library> getLibraryByName(String name) {
         return libraryRepository.findByName(name);
     }
-    public Library postLibrary(Library library){
+
+    public Library postLibrary(Library library) {
         return libraryRepository.save(library);
     }
-    public Library patchLibraryName(Long id,String name){
-        Library library=libraryRepository.findById(id).orElseThrow(()->new LibraryException("No lib with that id"));
+
+    public Library patchLibraryName(Long id, String name) {
+        Library library = libraryRepository.findById(id).orElseThrow(() -> new LibraryException("No lib with that id"));
         library.setName(name);
         return libraryRepository.save(library);
     }
